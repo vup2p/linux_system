bbe5b1ffc34b9816f0a05db944baff54d4f560a9
vup2p <102939807+vup2p@users.noreply.github.com>
2023-02-26 21:55:00 +0700

    Update and rename check_port_local.sh to network/check_port_local.sh

---
+++ network/check_port_local.sh
@@ -0,0 +1,10 @@
+if [ ! -n $1 ]; then
+    echo "Please input port number. Ex: check_port_local 443"
+    exit 1
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":$1$"; then
+    echo -e "\e[31mInstallation is not possible, port $1 already in use.\e[39m"
+    exit 1
+else
+    echo -e "Port $1 isn't use. You can use"
+fi
