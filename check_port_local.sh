2cad65e1afa0ba864444cf0bd1d3b583bddbce59
vup2p <102939807+vup2p@users.noreply.github.com>
2023-02-26 16:33:23 +0700

    Update check_port_local.sh

--- check_port_local.sh
+++ check_port_local.sh
@@ -1,5 +1,5 @@
 check_port_local() {
-    if [ -n $1 ]; then
+    if [ ! -n $1 ]; then
         echo "Please input port number. Ex: check_port 443"
         exit 1
     fi
@@ -7,7 +7,7 @@ check_port_local() {
         echo -e "\e[31mInstallation is not possible, port $1 already in use.\e[39m"
         exit 1
     else
-        echo -e "Port $1 isn't use"
+        echo -e "Port $1 isn't use. You can use"
     fi
 }
 check_port_local $1
